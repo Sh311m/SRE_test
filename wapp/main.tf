59e418e06fda7ce0cb0da706a522fda56200476a
Sh311m <61997468+Sh311m@users.noreply.github.com>
2020-04-06 14:47:48 +0300

    add hello world app

---
+++ wapp/main.tf
@@ -0,0 +1,84 @@
+provider "kubernetes" {}
+
+data "local_file" "news_api_key" {
+  filename = "${path.module}/../private/.news_api_key"
+}
+
+#resource "kubernetes_secret" "wapp-api-key" {
+#  metadata {
+#    name = "wapp_api_key"
+#  }
+#
+#  data = {
+#    "key_txt" = "${file("${path.module}/.new_api_key")}"
+#  }
+#
+#  type = "kubernetes.io/wapp-api-key"
+#}
+
+resource "kubernetes_service" "web_app" {
+  metadata {
+    name = var.wapp_image_name
+  }
+  spec {
+    selector = {
+      app = "${kubernetes_deployment.web_app_dep.metadata.0.labels.app}"
+    }
+    port {
+      port        = 80
+      target_port = 3000
+    }
+
+    type = "NodePort"
+  }
+}
+
+resource "kubernetes_deployment" "web_app_dep" {
+  metadata {
+    name = var.wapp_image_name
+    labels = {
+      app = var.wapp_image_name
+    }
+  }
+
+  spec {
+    replicas = 1
+    selector {
+        match_labels = {
+            app = var.wapp_image_name
+        }
+    }
+    template {
+        metadata {
+            labels = {
+                app = var.wapp_image_name
+            }
+        }
+
+        spec {
+            container {
+              image = "${var.wapp_gcr}/${var.wapp_image_name}:latest"
+              name  = var.wapp_image_name
+
+              port {
+                container_port = 3000
+              }
+              env {
+                name = "API_KEY"
+                value = "${data.local_file.news_api_key.content}"
+              }
+              resources {
+                  limits {
+                    cpu    = "0.5"
+                    memory = "512Mi"
+                  }
+                  requests {
+                    cpu    = "250m"
+                    memory = "50Mi"
+                  }
+              }
+           }
+        }
+    }
+  }
+}
---
+++ wapp/variables.tf
@@ -0,0 +1,9 @@
+variable "wapp_gcr" {
+    type = string
+}
+
+variable "wapp_image_name" {
+    type = string
+}
+
+
